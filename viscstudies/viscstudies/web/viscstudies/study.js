5ea5699b1807b541a20dcca4116599983c8b4c10
Adam Rauch <adam@labkey.com>
2009-12-04 20:56:39 +0000

    Update licenses.
    SVN r12487 |2009-12-04 20:56:39 +0000

--- viscstudies/viscstudies/web/viscstudies/study.js
+++ viscstudies/viscstudies/web/viscstudies/study.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2009 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 var _allStudies = null;
 var _selectedStudies = null;
 var _isViscUser = false;
