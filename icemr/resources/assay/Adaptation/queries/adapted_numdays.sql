41723eb41b9e1ae70fcd8b7fb3cb3ff15f8deefc
Dax Hawkins <dax@labkey.com>
2013-01-29 21:51:59 +0000

    icemr:  tweak the default results view to show daily maintenance instead of the flask summary.  The flask summary including the calculated columns can be by clicking the link for the sample/flask id column.  Also include all the flask columns plus the calculated columns in the summary view.
    SVN r24349 |2013-01-29 21:51:59 +0000

--- icemr/resources/assay/Adaptation/queries/adapted_numdays.sql
+++ icemr/resources/assay/Adaptation/queries/adapted_numdays.sql
@@ -1,10 +1,11 @@
 -- combines the results from the two calculated queries and combines them with the
 -- over flask sample set data
 SELECT
-f.PatientID, f.SampleID, f.Scientist, f.MaintenanceStopped, f.MaintenanceDate,
+f.PatientID, f.SampleID, f.Scientist, f.Stage, f.Parasitemia, f.Gametocytemia, f.PatientpRBCs, f.Hematocrit,
+f.CultureMedia, f.SerumBatchID, f.AlbumaxBatchID, f.FoldIncrease1, f.FoldIncrease2, f.FoldIncrease3,
+f.AdaptationCriteria,f.Comments, f.MaintenanceStopped, f.MaintenanceDate,
 a.IncreaseTest1, a.IncreaseTest2, a.IncreaseTest3, a.PassTest1, a.PassTest2, a.PassTest3, a.SuccessfulAdaptation,
 d.NumDaysInCulture, d.NumDaysToGrowthTestStart, d.NumDaysFromGrowthTestFinishToAdaptation, d.NumDaysToAdaptation,
-f.Comments
 FROM
 Samples.Flasks as f,
 stored_parasitemia_flask_adapted as a,
