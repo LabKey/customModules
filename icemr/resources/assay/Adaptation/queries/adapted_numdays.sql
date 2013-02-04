38e98eb5f0f9ff66ad57da8de86b04a0340ea485
Dax Hawkins <dax@labkey.com>
2013-02-04 06:00:01 +0000

    icemr module:  add AdaptationDate to adapt_numdays query.  Add verification for this column in tests.
    SVN r24453 |2013-02-04 06:00:01 +0000

--- icemr/resources/assay/Adaptation/queries/adapted_numdays.sql
+++ icemr/resources/assay/Adaptation/queries/adapted_numdays.sql
@@ -5,7 +5,7 @@ f.PatientID, f.SampleID, f.Scientist, f.Stage, f.Parasitemia, f.Gametocytemia, f
 f.CultureMedia, f.SerumBatchID, f.AlbumaxBatchID, f.FoldIncrease1, f.FoldIncrease2, f.FoldIncrease3,
 f.AdaptationCriteria,f.Comments, f.MaintenanceStopped, f.MaintenanceDate,
 a.IncreaseTest1, a.IncreaseTest2, a.IncreaseTest3, a.PassTest1, a.PassTest2, a.PassTest3, a.SuccessfulAdaptation,
-d.NumDaysInCulture, d.NumDaysToGrowthTestStart, d.NumDaysFromGrowthTestFinishToAdaptation, d.NumDaysToAdaptation,
+f.AdaptationDate, d.NumDaysInCulture, d.NumDaysToGrowthTestStart, d.NumDaysFromGrowthTestFinishToAdaptation, d.NumDaysToAdaptation,
 FROM
 Samples.Flasks as f,
 stored_parasitemia_flask_adapted as a,
