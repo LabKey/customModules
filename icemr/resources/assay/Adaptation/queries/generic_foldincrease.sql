ca01759e1c901cff531b30d0a024d059424d28f5
Dax Hawkins <dax@labkey.com>
2013-02-06 23:02:22 +0000

    icemr: add buttons for generic foldIncrease and numDays calculators, tweak calculators based on client feedback, add automated tests for the two generic calculators
    SVN r24524 |2013-02-06 23:02:22 +0000

--- icemr/resources/assay/Adaptation/queries/generic_foldincrease.sql
+++ icemr/resources/assay/Adaptation/queries/generic_foldincrease.sql
@@ -13,14 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-PARAMETERS(start TIMESTAMP, finish TIMESTAMP)
+PARAMETERS(start TIMESTAMP, finish TIMESTAMP, flaskId VARCHAR)
 SELECT f.*, i.ParasitemiaStart, i.ParasitemiaFinish, i.Increase
 FROM
   (SELECT ps.SampleID, ps.Parasitemia as ParasitemiaStart, pf.Parasitemia as ParasitemiaFinish,
   round((pf.parasitemia/ps.parasitemia),2)  As Increase
     FROM
-      (SELECT  SampleID,  Parasitemia FROM  Data  WHERE  (timestampdiff('SQL_TSI_DAY', MeasurementDate, start)=0))  as ps,
-      (SELECT  SampleID,  Parasitemia FROM  Data  WHERE  (timestampdiff('SQL_TSI_DAY', MeasurementDate, finish)=0))as pf
+      (SELECT  SampleID,  Parasitemia FROM  Data  WHERE  (timestampdiff('SQL_TSI_DAY', MeasurementDate, start)=0)
+        AND SampleID = flaskId)  as ps,
+      (SELECT  SampleID,  Parasitemia FROM  Data  WHERE  (timestampdiff('SQL_TSI_DAY', MeasurementDate, finish)=0)
+        AND SampleID = flaskId) as pf
     WHERE ps.SampleID = pf.SampleID) as i,
   Samples.Flasks as f
 WHERE i.SampleID = f.SampleID
