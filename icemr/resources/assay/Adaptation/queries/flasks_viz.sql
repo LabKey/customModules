63b8ad881ddfc5ffccafc7b173c41e0034968317
Dax Hawkins <dax@labkey.com>
2013-02-19 20:19:59 +0000

    icemr: add visualization query to combine columns from run, data, and flasks sampleset
    SVN r24741 |2013-02-19 20:19:59 +0000

---
+++ icemr/resources/assay/Adaptation/queries/flasks_viz.sql
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ -- combines results from flasks, runs, and daily maintenance into
+ -- a table for visualization
+
+SELECT f.SampleID, StartDate as MeasurementDate, f.Stage, f.Parasitemia, f.Gametocytemia, f.PatientpRBCs, f.Hematocrit,
+    f.CultureMedia, NULL as Removed, NULL as GrowthFoldTestInitiated, NULL as GrowthFoldTestFinished,
+    NULL As Contaminiation, NULL as MycoTestResults, NULL as FeezerProIDS, NULL as FlaskMaintenanceStopped
+FROM Runs, Samples.Flasks as f
+WHERE Runs.PatientID = f.PatientID
+
+UNION ALL
+
+SELECT SampleID, MeasurementDate, Stage, Parasitemia, Gametocytemia, NULL As PatientpRBCs, NULL as Hematocrit,
+    NULL As CultureMedia, Removed, GrowthFoldTestInitiated, GrowthFoldTestFinished, Contamination, MycoTestResult,
+    FreezerProIDs, FlaskMaintenanceStopped
+FROM Data
+
+
+
