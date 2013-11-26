041d5578a471dd3a2314827d37b3ce6e1a2b095e
Dax Hawkins <dax@labkey.com>
2013-11-26 19:24:49 +0000

    merge from modules13.3  (29652-29673) including release13.3 (29646-29663)
    SVN r29674 |2013-11-26 19:24:49 +0000

---
+++ ms2extensions/resources/queries/ms2/CometQualifiedPeptideCountsPerRun.sql
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2012-2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+SELECT P.Fraction.Run.run AS MS2Run, COUNT(*) AS TotalPeptides
+, COUNT(DISTINCT P.Peptide) AS DistinctPeptides
+FROM CometPeptides P
+WHERE ((Charge=1 AND Xcorr >= 1.8)
+    OR (Charge=2 AND Xcorr >= 2.3)
+    OR (Charge=3 AND Xcorr >= 2.8)
+    OR (Charge=4 AND Xcorr >= 3.3))
+AND (DeltaCn >= 0.1)
+GROUP BY P.Fraction.Run.run;
---
+++ ms2extensions/resources/queries/ms2extensions/GetCometPeptideCounts.sql
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+SELECT
+  r.MS2Details AS ms2Run,
+  COALESCE(X.totalPeptides, 0) AS totalPeptides,
+  COALESCE(X.distinctPeptides, 0) as distinctPeptides
+FROM
+  ms2.MS2SearchRuns r
+LEFT OUTER JOIN
+  (SELECT
+    P.Fraction.Run.run AS ms2Run,
+    COUNT(*) AS totalPeptides,
+    COUNT(DISTINCT P.Peptide) AS distinctPeptides,
+  FROM ms2.CometPeptides P
+  WHERE (DeltaCn >= 0.1) AND ((Charge=1 AND Xcorr >= 1.8)
+      OR (Charge=2 AND Xcorr >= 2.3)
+      OR (Charge=3 AND Xcorr >= 2.8)
+      OR (Charge=4 AND Xcorr >= 3.3))
+  GROUP BY P.Fraction.Run.run
+) X
+ON r.MS2Details = X.ms2Run
