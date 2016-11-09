b4298a240f3c75bcc8dd12cdb815d3957ff2fcb6
Trey Chadick <tchad@labkey.com>
2016-11-09 19:29:10 +0000

    Study name isn't actually a folder name
    SVN r47563 |2016-11-09 19:29:10 +0000

--- viscstudies/test/src/org/labkey/test/tests/viscstudies/CAVDStudyTest.java
+++ viscstudies/test/src/org/labkey/test/tests/viscstudies/CAVDStudyTest.java
@@ -493,7 +493,7 @@ public class CAVDStudyTest extends StudyBaseTest
             assertElementPresent(Locator.tagWithAttribute("img", "src", status[1]), 1);
         }
         log("Verify that you can navigate to study and set status from study list.");
-        clickFolder(study3name);
+        clickAndWait(Locator.linkWithText(study3name));
         // wait for the study schedule grid to load, any dataset name will do
         waitForText(DATASETS.values().iterator().next());
         // set the status to Locked for all of the datasets
