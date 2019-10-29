b2a1a9ed154528210f344ac3e04f976e0d09f8d7
Cory Nathe <cnathe@labkey.com>
2019-10-28 19:17:27 -0500

    Item 6204: ICEMRModuleTest update to use new assay designer for assay creation (#12)

--- icemr/test/src/org/labkey/test/tests/icemr/ICEMRModuleTest.java
+++ icemr/test/src/org/labkey/test/tests/icemr/ICEMRModuleTest.java
@@ -27,11 +27,11 @@ import org.labkey.test.TestFileUtils;
 import org.labkey.test.TestTimeoutException;
 import org.labkey.test.WebTestHelper;
 import org.labkey.test.categories.CustomModules;
-import org.labkey.test.components.PropertiesEditor;
+import org.labkey.test.components.domain.DomainFormPanel;
+import org.labkey.test.params.FieldDefinition;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.ExcelHelper;
 import org.labkey.test.util.Ext4Helper;
-import org.labkey.test.util.ListHelper;
 import org.labkey.test.util.LogMethod;
 import org.labkey.test.util.LoggedParam;
 import org.labkey.test.util.PortalHelper;
@@ -213,10 +213,10 @@ public class ICEMRModuleTest extends BaseWebDriverTest
         ph.addWebPart("Sample Sets");
         ph.addWebPart("ICEMR Upload Tests");
 
-        _assayHelper.createAssayWithDefaults(DIAGNOSTICS_ASSAY_DESIGN, DIAGNOSTIC_ASSAY_NAME);
-        _assayHelper.createAssayWithDefaults(TRACKING_ASSAY_DESIGN, ADAPTATION_ASSAY_NAME);
-        _assayHelper.createAssayWithDefaults(TRACKING_ASSAY_DESIGN, SELECTION_ASSAY_NAME);
-        _assayHelper.createAssayWithDefaults(SPECIES_ASSAY_DESIGN, SPECIES_ASSAY_NAME);
+        _assayHelper.createAssayDesignWithDefaults(DIAGNOSTICS_ASSAY_DESIGN, DIAGNOSTIC_ASSAY_NAME);
+        _assayHelper.createAssayDesignWithDefaults(TRACKING_ASSAY_DESIGN, ADAPTATION_ASSAY_NAME);
+        _assayHelper.createAssayDesignWithDefaults(TRACKING_ASSAY_DESIGN, SELECTION_ASSAY_NAME);
+        _assayHelper.createAssayDesignWithDefaults(SPECIES_ASSAY_DESIGN, SPECIES_ASSAY_NAME);
         createFlasksSampleSet(ADAPTATION_FLASKS_NAME, ADAPTATION_FLASK_FILE);
         createFlasksSampleSet(SELECTION_FLASKS_NAME, SELECTION_FLASK_FILE);
         goToProjectHome();
@@ -754,19 +754,19 @@ public class ICEMRModuleTest extends BaseWebDriverTest
         sampleHelper.createSampleSet(sampleSetName, "${SampleID}");
 
         String sampleSetCols = TestFileUtils.getFileContents(TestFileUtils.getSampleData(sampleSetFilename));
-        addFieldsNoImport(sampleSetCols);
+        DomainFormPanel domainFormPanel = addFieldsNoImport(sampleSetCols);
 
-      // waitForElement(Locator.xpath("//input[@name='ff_label3']"), WAIT_FOR_JAVASCRIPT);
         // set the scientist column type to a user instead of just an int
         // this will make it be a combobox in the drop down.
-        setFormElement(Locator.xpath("//input[@name='ff_type2']"), "User");
+        domainFormPanel.getField(2).setType(FieldDefinition.ColumnType.User);
+
         clickButton("Save");
         clickProject(getProjectName());
     }
 
-    private void addFieldsNoImport(String fieldList)
+    private DomainFormPanel addFieldsNoImport(String fieldList)
     {
-        PropertiesEditor propertiesEditor = new PropertiesEditor.PropertiesEditorFinder(getDriver()).withTitle("Field Properties").waitFor();
+        DomainFormPanel domainFormPanel = new DomainFormPanel.DomainFormPanelFinder(getDriver()).waitFor();
         Scanner reader = new Scanner(fieldList);
         while (reader.hasNextLine())
         {
@@ -791,19 +791,22 @@ public class ICEMRModuleTest extends BaseWebDriverTest
             String description = lineReader.hasNext() ? lineReader.next() : "";
 
             if (type.equals("http://www.w3.org/2001/XMLSchema#string")) type = "String";
-            if (type.equals("http://www.w3.org/2001/XMLSchema#double")) type = "Double";
+            if (type.equals("http://www.w3.org/2001/XMLSchema#double")) type = "Decimal";
             if (type.equals("http://www.w3.org/2001/XMLSchema#int")) type = "Integer";
-            if (type.equals("http://www.w3.org/2001/XMLSchema#dateTime")) type = "DateTime";
+            if (type.equals("http://www.w3.org/2001/XMLSchema#dateTime")) type = "DateAndTime";
             if (type.equals("http://www.w3.org/2001/XMLSchema#multiLine")) type = "MultiLine";
             if (type.equals("http://www.w3.org/2001/XMLSchema#boolean")) type = "Boolean";
 
-            ListHelper.ListColumnType typeEnum = ListHelper.ListColumnType.valueOf(type);
-            ListHelper.ListColumn newCol = new ListHelper.ListColumn(name, label, typeEnum, description);
+            FieldDefinition field = new FieldDefinition(name, FieldDefinition.ColumnType.valueOf(type))
+                .setLabel(label)
+                .setDescription(description);
 
-            if (required.equals("TRUE")) newCol.setRequired(true);
-            if (mvenabled.equals("TRUE")) newCol.setMvEnabled(true);
-            propertiesEditor.addField(newCol);
+            if (required.equals("TRUE")) field.setRequired(true);
+            if (mvenabled.equals("TRUE")) field.setMvEnabled(true);
+            domainFormPanel.addField(field);
         }
+
+        return domainFormPanel;
     }
 
     @LogMethod
