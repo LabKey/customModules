554c7624a3ae67f5bd750e6852c38764f0d8fb9d
labkey-martyp <martyp@labkey.com>
2020-08-04 16:26:09 -0700

    Revert test change

--- viscstudies/test/src/org/labkey/test/tests/viscstudies/CAVDStudyTest.java
+++ viscstudies/test/src/org/labkey/test/tests/viscstudies/CAVDStudyTest.java
@@ -571,7 +571,7 @@ public class CAVDStudyTest extends StudyBaseTest
     {
         navigateToFolder(PROJECT_NAME, folderName);
         goToSchemaBrowser();
-        selectQuery("viscstudies", "Study");
+        selectQuery("viscstudies", "studies");
         waitForText("view data");
         clickAndWait(Locator.linkContainingText("view data"));
     }
