fa3cdba2790e08725b7a4b00114bafa6847c3728
Adam Rauch <adam@labkey.com>
2024-06-13 14:52:53 -0700

    Move inlineViewDesigner.js

--- ms2extensions/src/org/labkey/ms2extensions/RunGridWebPart.java
+++ ms2extensions/src/org/labkey/ms2extensions/RunGridWebPart.java
@@ -39,7 +39,7 @@ public class RunGridWebPart extends VBox
         setTitle("MS2 Runs With Peptide Counts");
 
         JspView<ViewContext> v = new JspView<>("/org/labkey/ms2extensions/runGridFilters.jsp", viewContext);
-        addClientDependencies(Collections.singleton(ClientDependency.fromPath("/MS2/inlineViewDesigner.js")));
+        addClientDependencies(Collections.singleton(ClientDependency.fromPath("/protein/inlineViewDesigner.js")));
         addView(v);
 
         UserSchema ms2Schema = MS2Service.get().createSchema(viewContext.getUser(), viewContext.getContainer());
