15b04159e0e0338dd1048877c4b638160db36650
Adam Rauch <adam@labkey.com>
2026-01-01 08:58:50 -0800

    Bump schema versions to 26.000 (#245)

--- ms2extensions/src/org/labkey/ms2extensions/MS2ExtensionsModule.java
+++ ms2extensions/src/org/labkey/ms2extensions/MS2ExtensionsModule.java
@@ -64,7 +64,7 @@ public class MS2ExtensionsModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 25.000;
+        return 26.000;
     }
 
     @Override
