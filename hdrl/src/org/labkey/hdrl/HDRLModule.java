907c6305cb7028b5c7add889bf565f2f7b000e00
labkey-jeckels <jeckels@labkey.com>
2026-01-23 16:00:19 -0800

    Make select inactive modules unreachable

--- hdrl/src/org/labkey/hdrl/HDRLModule.java
+++ hdrl/src/org/labkey/hdrl/HDRLModule.java
@@ -73,6 +73,12 @@ public class HDRLModule extends DefaultModule
     }
 
     @Override
+    public boolean isAvailableOnlyWhenActive()
+    {
+        return true;
+    }
+
+    @Override
     public void doStartup(ModuleContext moduleContext)
     {
         // add a container listener so we'll know when our container is deleted:
