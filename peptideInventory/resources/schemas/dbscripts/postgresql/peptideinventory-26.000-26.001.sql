6d8d66f0b59189155f7f8ab1445988f5861f0516
Adam Rauch <adam@labkey.com>
2026-05-25 15:01:43 -0700

    Update copyrights and licenses

--- peptideInventory/resources/schemas/dbscripts/postgresql/peptideinventory-26.000-26.001.sql
+++ peptideInventory/resources/schemas/dbscripts/postgresql/peptideinventory-26.000-26.001.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2026 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 -- Converting uq_lotassignment [peptideId, container, lotNumber] from unique to non-unique index because pk_lotassignment [peptideId, container] overlaps it with a smaller column set
 ALTER TABLE peptideinventory.lotAssignment DROP CONSTRAINT uq_lotassignment;
 CREATE INDEX ix_lotassignment ON peptideinventory.lotAssignment(peptideId, container, lotNumber);
